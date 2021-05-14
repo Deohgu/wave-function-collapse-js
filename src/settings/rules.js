6c0abca25adadcd964260634807c5b8f4e2fac13
Deohgu <dioggopsanttos@gmail.com>
2021-05-14 23:56:38 +0100

    Adds non-syntax rules

--- src/settings/rules.js
+++ src/settings/rules.js
@@ -1,6 +1,8 @@
 export default {
-  water: { colour: "blue" },
-  coast: { colour: "light brown" },
+  water: { colour: "blue",
+    softRules: "Water can only be near Coast or itself"
+  },
+  coast: { colour: "light brown", softRules: "Coast must have Land on one side and Sea on the other" },
   land: { colour: "green" },
-  mountain: { colour: "dark grey" },
+  mountain: { colour: "dark grey", softRules: "Mountain can only be near Land or itself" },
 };
