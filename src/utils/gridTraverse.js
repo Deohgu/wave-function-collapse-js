e4931387cc6bb79a8495cb8882896ee6dcc7a4f2
Deohgu <dioggopsanttos@gmail.com>
2021-05-19 23:05:10 +0100

    Cleans up comments

--- src/utils/gridTraverse.js
+++ src/utils/gridTraverse.js
@@ -1,19 +1,10 @@
 // CURRENT:
 // Define a way to traverse the grid and check based on the rules
-// QUESTION:
-// Should the original grid container the various identities already there? Very likely!
-// No point having it empty and then running through identities state in Cell.
 
 export default (arr) => {
   // arr.forEach((curr) => {
   //   console.log(curr);
   // });
 
-  // console.log("identities: ", identities);
   console.log("gridTraverve, arr: ", arr);
-
-  // water, coast, land, mountain
-  // for (const curr in identities) {
-  //   console.log(curr);
-  // }
 };
