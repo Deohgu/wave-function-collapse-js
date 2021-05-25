801b6b9e6aac6dfa1b49953216fa8b161b436b47
Deohgu <dioggopsanttos@gmail.com>
2021-05-25 15:20:10 +0100

    Comments out code, adds note for crashing error

--- src/utils/gridTraverse.js
+++ src/utils/gridTraverse.js
@@ -14,35 +14,38 @@
 export default (array, x, y) => {
   // const arrClone = [...array];
 
+  //  Need to figure out a way for these not to crash when x and y is equal to 0.
+  //    Perhaps like before, make it a function that receives x and y arguments
+  //      Or BETTER YET!, have it as an object but the keys are methods that key called in the if statements when needed
+  //      thus never using something when it will crash
   // If not against a wall
-  // Check Minesweeper repository on how I solved this issue
   // x and y are equal to the index in the loops which determines the current cell
   // eslint-disable-next-line no-unused-vars
-  const allDirections = {
-    up: array[y - 1][x],
-    right: array[y][x + 1],
-    down: array[y + 1][x],
-    left: array[y][x - 1],
-  };
+  // const allDirections = {
+  //   up: array[y - 1][x],
+  //   right: array[y][x + 1],
+  //   down: array[y + 1][x],
+  //   left: array[y][x - 1],
+  // };
 
   //  Directions for each position in each if section
   // eslint-disable-next-line no-unused-vars
-  const conditionalDirections = {
-    topWall: [allDirections.right, allDirections.down, allDirections.left],
-    rightWall: [allDirections.up, allDirections.down, allDirections.left],
-    topRightCorner: [allDirections.down, allDirections.left],
-    bottomCorner: [allDirections.up, allDirections.left],
-    bottomWall: [allDirections.up, allDirections.right, allDirections.left],
-    leftBottomCorner: [allDirections.up, allDirections.right],
-    leftWall: [allDirections.up, allDirections.right, allDirections.down],
-    topLeftCorner: [allDirections.right, allDirections.down],
-    noWall: [
-      allDirections.up,
-      allDirections.right,
-      allDirections.down,
-      allDirections.left,
-    ],
-  };
+  // const conditionalDirections = {
+  //   topWall: [allDirections.right, allDirections.down, allDirections.left],
+  //   rightWall: [allDirections.up, allDirections.down, allDirections.left],
+  //   topRightCorner: [allDirections.down, allDirections.left],
+  //   bottomCorner: [allDirections.up, allDirections.left],
+  //   bottomWall: [allDirections.up, allDirections.right, allDirections.left],
+  //   leftBottomCorner: [allDirections.up, allDirections.right],
+  //   leftWall: [allDirections.up, allDirections.right, allDirections.down],
+  //   topLeftCorner: [allDirections.right, allDirections.down],
+  //   noWall: [
+  //     allDirections.up,
+  //     allDirections.right,
+  //     allDirections.down,
+  //     allDirections.left,
+  //   ],
+  // };
 
   // Find if against the wall
   //  TOP WALL
@@ -63,5 +66,5 @@ export default (array, x, y) => {
   //   });
   // });
 
-  console.log("gridTraverve, array, current: ", array, current);
+  console.log("gridTraverve, array, x, y: ", array, x, y);
 };
