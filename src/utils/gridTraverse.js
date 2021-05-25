a8a9ef8721746320c485a78ef57e0ac98c5cf045
Deohgu <dioggopsanttos@gmail.com>
2021-05-25 22:44:49 +0100

    Lays down conditionals for the nine potential positions

--- src/utils/gridTraverse.js
+++ src/utils/gridTraverse.js
@@ -14,50 +14,76 @@
 export default (array, x, y) => {
   // const arrClone = [...array];
 
-  //  Need to figure out a way for these not to crash when x and y is equal to 0.
-  //    Perhaps like before, make it a function that receives x and y arguments
-  //      Or BETTER YET!, have it as an object but the keys are methods that key called in the if statements when needed
-  //      thus never using something when it will crash
   // If not against a wall
   // x and y are equal to the index in the loops which determines the current cell
   // eslint-disable-next-line no-unused-vars
-  // const allDirections = {
-  //   up: array[y - 1][x],
-  //   right: array[y][x + 1],
-  //   down: array[y + 1][x],
-  //   left: array[y][x - 1],
-  // };
+  const allDirections = {
+    up() {
+      return array[y - 1][x];
+    },
+    right() {
+      return array[y][x + 1];
+    },
+    down() {
+      return array[y + 1][x];
+    },
+    left() {
+      return array[y][x - 1];
+    },
+  };
 
   //  Directions for each position in each if section
   // eslint-disable-next-line no-unused-vars
-  // const conditionalDirections = {
-  //   topWall: [allDirections.right, allDirections.down, allDirections.left],
-  //   rightWall: [allDirections.up, allDirections.down, allDirections.left],
-  //   topRightCorner: [allDirections.down, allDirections.left],
-  //   bottomCorner: [allDirections.up, allDirections.left],
-  //   bottomWall: [allDirections.up, allDirections.right, allDirections.left],
-  //   leftBottomCorner: [allDirections.up, allDirections.right],
-  //   leftWall: [allDirections.up, allDirections.right, allDirections.down],
-  //   topLeftCorner: [allDirections.right, allDirections.down],
-  //   noWall: [
-  //     allDirections.up,
-  //     allDirections.right,
-  //     allDirections.down,
-  //     allDirections.left,
-  //   ],
-  // };
+  const conditionalDirections = {
+    topWall: [allDirections.right, allDirections.down, allDirections.left],
+    rightWall: [allDirections.up, allDirections.down, allDirections.left],
+    topRightCorner: [allDirections.down, allDirections.left],
+    bottomCorner: [allDirections.up, allDirections.left],
+    bottomWall: [allDirections.up, allDirections.right, allDirections.left],
+    leftBottomCorner: [allDirections.up, allDirections.right],
+    leftWall: [allDirections.up, allDirections.right, allDirections.down],
+    topLeftCorner: [allDirections.right, allDirections.down],
+    noWall: [
+      allDirections.up,
+      allDirections.right,
+      allDirections.down,
+      allDirections.left,
+    ],
+  };
 
-  // Find if against the wall
-  //  TOP WALL
-  //    if (y === 0)
-  //  RIGHT WALL
-  //    if (x === array[0].length - 1)
-  //  BOTTOM WALL
-  //    if (y === array.length - 1)
-  //  LEFT WALL
-  //    if (x === 0)
-  //  CORNERS
-  //    Chain wall conditionals. e.g: if (top wall) if (right wall) run right corner code if (left wall)
+  //  Starts collapsing
+  if (y === 0) {
+    //  TOP RIGHT CORNER
+    if (x === array[0].length - 1) {
+      console.log("TOP RIGHT CORNER");
+      //  TOP LEFT CORNER
+    } else if (x === 0) {
+      console.log("TOP LEFT CORNER");
+      //  TOP WALL
+    } else {
+      console.log("TOP WALL");
+    }
+  } else if (y === array.length - 1) {
+    //  BOTTOM RIGHT CORNER
+    if (x === array[0].length - 1) {
+      console.log("BOTTOM LEFT CORNER");
+      // BOTTOM LEFT CORNER
+    } else if (x === 0) {
+      console.log("BOTTOM RIGHT CORNER");
+      //  BOTTOM WALL
+    } else {
+      console.log("BOTTOM WALL");
+    }
+    //  RIGHT WALL ONLY
+  } else if (x === array[0].length - 1) {
+    console.log("RIGHT WALL");
+    //  LEFT WALL ONLY
+  } else if (x === 0) {
+    console.log("LEFT WALL");
+    // AWAY FROM WALL
+  } else {
+    console.log("AWAY FROM WALL");
+  }
 
   // Traverses entire array
   // arrClone.forEach((arrY) => {
