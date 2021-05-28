1341e45fa8b8d8626d2842be0b84bce5de2e9b52
Deohgu <dioggopsanttos@gmail.com>
2021-05-28 22:49:19 +0100

    Improves naming, and lays down more instructions

--- src/utils/gridTraverse.js
+++ src/utils/gridTraverse.js
@@ -11,7 +11,7 @@
 //  ForEach of that neighboorArray call gridTraverse
 /////////////
 
-export const gridTraverse = (array, x, y) => {
+export const gridTraverse = (array, y, x) => {
   // eslint-disable-next-line no-unused-vars
   const arrayClone = [...array];
 
@@ -19,16 +19,16 @@ export const gridTraverse = (array, x, y) => {
   // x and y are equal to the index in the loops which determines the current cell
   // eslint-disable-next-line no-unused-vars
   const allDirections = {
-    up() {
+    north() {
       return array[y - 1][x];
     },
-    right() {
+    east() {
       return array[y][x + 1];
     },
-    down() {
+    south() {
       return array[y + 1][x];
     },
-    left() {
+    west() {
       return array[y][x - 1];
     },
   };
@@ -36,19 +36,19 @@ export const gridTraverse = (array, x, y) => {
   //  Directions for each position in each if section
   // eslint-disable-next-line no-unused-vars
   const conditionalDirections = {
-    topWall: [allDirections.right, allDirections.down, allDirections.left],
-    rightWall: [allDirections.up, allDirections.down, allDirections.left],
-    topRightCorner: [allDirections.down, allDirections.left],
-    bottomCorner: [allDirections.up, allDirections.left],
-    bottomWall: [allDirections.up, allDirections.right, allDirections.left],
-    leftBottomCorner: [allDirections.up, allDirections.right],
-    leftWall: [allDirections.up, allDirections.right, allDirections.down],
-    topLeftCorner: [allDirections.right, allDirections.down],
+    northWall: [allDirections.east, allDirections.south, allDirections.west],
+    northEastCorner: [allDirections.south, allDirections.west],
+    eastWall: [allDirections.north, allDirections.south, allDirections.west],
+    southEastCorner: [allDirections.north, allDirections.west],
+    southWall: [allDirections.north, allDirections.east, allDirections.west],
+    southWestCorner: [allDirections.north, allDirections.east],
+    westWall: [allDirections.north, allDirections.east, allDirections.south],
+    northWestCorner: [allDirections.east, allDirections.south],
     noWall: [
-      allDirections.up,
-      allDirections.right,
-      allDirections.down,
-      allDirections.left,
+      allDirections.north,
+      allDirections.east,
+      allDirections.south,
+      allDirections.west,
     ],
   };
 
@@ -56,19 +56,37 @@ export const gridTraverse = (array, x, y) => {
 
   //  Starts collapsing
   if (y === 0) {
-    //  TOP RIGHT CORNER
+    //  TOP east CORNER
     if (x === array[0].length - 1) {
-      console.log("TOP RIGHT CORNER");
-      //  TOP LEFT CORNER
+      console.log("TOP east CORNER");
+      //  TOP west CORNER
     } else if (x === 0) {
-      console.log("TOP LEFT CORNER");
+      console.log("TOP west CORNER");
       // eslint-disable-next-line no-unused-vars
-      conditionalDirections.topLeftCorner.forEach((direction) => {
+      conditionalDirections.northWestCorner.forEach((direction) => {
         //  Runs through all potential directions
         // eslint-disable-next-line no-unused-vars
-        direction.forEach((identity) => {
+        direction().forEach((identity) => {
+          //  Can I use the
+          console.log(
+            "arrayClone[y][x][0][1].rules: ",
+            arrayClone[y][x][0][1].rules
+          );
+
+          console.log("direction, identity: ", direction(), identity);
           //  Loops through each direction identity
           //    Compares arrayClone[y][x][0] with each identity
+
+          //  FIXME:
+          //  ERROR HERE
+          //    If rules array of current identity to that direction has identity being analised then do something
+          //      Meaning, ogIdentities(matching this one).rules.(direction we are looking).hasIndexOf(identity)
+          if (identity.indexOf(arrayClone[y][x][0])) {
+            console.log(
+              "TESTING indexOf: ",
+              identity.indexOf(arrayClone[y][x][0])
+            );
+          }
           //    If identity can not be in that direction split from arrayClone
           //    If at least one identity was split, add to the back line of neighboursCue the current location of the block
         });
@@ -79,22 +97,22 @@ export const gridTraverse = (array, x, y) => {
       console.log("TOP WALL");
     }
   } else if (y === array.length - 1) {
-    //  BOTTOM RIGHT CORNER
+    //  south east CORNER
     if (x === array[0].length - 1) {
-      console.log("BOTTOM LEFT CORNER");
-      // BOTTOM LEFT CORNER
+      console.log("south west CORNER");
+      // south west CORNER
     } else if (x === 0) {
-      console.log("BOTTOM RIGHT CORNER");
-      //  BOTTOM WALL
+      console.log("south east CORNER");
+      //  south WALL
     } else {
-      console.log("BOTTOM WALL");
+      console.log("south WALL");
     }
-    //  RIGHT WALL ONLY
+    //  east WALL ONLY
   } else if (x === array[0].length - 1) {
-    console.log("RIGHT WALL");
-    //  LEFT WALL ONLY
+    console.log("east WALL");
+    //  west WALL ONLY
   } else if (x === 0) {
-    console.log("LEFT WALL");
+    console.log("west WALL");
     // AWAY FROM WALL
   } else {
     console.log("AWAY FROM WALL");
