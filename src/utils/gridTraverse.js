dca9f5fffb7dcc2533e36803f02681da1091d405
Deohgu <dioggopsanttos@gmail.com>
2021-05-30 16:35:17 +0100

    Fixes array deep clone and splits identity from original

--- src/utils/gridTraverse.js
+++ src/utils/gridTraverse.js
@@ -10,10 +10,8 @@
 //  Randomize order of equal numbers
 //  ForEach of that neighboorArray call gridTraverse
 /////////////
-import store from "@/store";
-
 export const gridTraverse = (array, y, x) => {
-  const arrayClone = [...array];
+  const arrayClone = JSON.parse(JSON.stringify(array));
 
   //  If not against a wall
   //  x and y are equal to the index in the loops which determines the current cell
@@ -67,9 +65,13 @@ export const gridTraverse = (array, y, x) => {
     noWall: ["north", "east", "south", "west"],
   };
 
+  //  Blocks that had identities split from it
+  //    Each block will have an indentity picked at random, split the others off and from the remaining one and call gridTraverse
+  //  Structure of values to be defined
   // const neighboursCue = []
 
   //  Starts collapsing
+  //  North Wall Section
   if (y === 0) {
     //  North East CORNER
     if (x === array[0].length - 1) {
@@ -79,31 +81,22 @@ export const gridTraverse = (array, y, x) => {
       console.log("North West CORNER");
       //  Loops valid directions for this particular location and returns then as a string
       conditionalDirections.northWestCorner.forEach((direction) => {
-        //  From the valid directions get the cells in that direction of original cell
-        console.log(
-          "TESTING: ",
-          arrayClone[allDirections[direction].y()][allDirections[direction].x()]
-        );
+        //  From the valid directions get the cell in that direction of original cell
         arrayClone[allDirections[direction].y()][
           allDirections[direction].x()
-        ].forEach((identity, index) => {
-          //  If the current identity in the current cell is not in the rules array of the original cell identity of the current direction, remove it from the current identity from the cell
+        ].forEach((identity) => {
+          //  If this identity, in this cell, is not in the rules of the original cell of its allowed neighbours of that direction remove it
           if (
             arrayClone[y][x][0][1].rules[direction].indexOf(identity[0]) === -1
           ) {
-            console.log(index);
-            // console.log(store.state.grid.grid[y][x][0]);
-            //  TODO:
             //  If identity can not be in that direction split from original array
 
-            //  FIXME:
-            //  Split based on current direction, somehow reuse the returned value from allDirections
-            console.log(
-              "store: ",
-              store.state.grid.grid[y][x].splice(index, 1)
-            );
-            // store.state.grid[][]
-            //  If at least one identity was split, add to the back line of neighboursCue the current location of the cell
+            //  Splices / mutates original array the current identity, thus instantly displays the mutation
+            //  How it works: Selects exact cell in which it currently is in,
+            array[allDirections[direction].y()][
+              allDirections[direction].x()
+            ].splice(array.indexOf(identity[0]), 1);
+            //  If at least one identity was split, add to the back line of neighboursCue to call the current location of the cell
           }
         });
         //  Loop through neighboursCue and call gridTraverse(arrayClone, )
@@ -112,6 +105,7 @@ export const gridTraverse = (array, y, x) => {
     } else {
       console.log("North WALL");
     }
+    //  South Wall Section
   } else if (y === array.length - 1) {
     //  South West CORNER
     if (x === array[0].length - 1) {
@@ -133,13 +127,4 @@ export const gridTraverse = (array, y, x) => {
   } else {
     console.log("AWAY FROM WALL");
   }
-
-  // Traverses entire array
-  // arrClone.forEach((arrY) => {
-  //   arrY.forEach((arrX) => {
-  //     console.log(arrX);
-  //   });
-  // });
-
-  console.log("gridTraverve, array, x, y: ", array, x, y);
 };
