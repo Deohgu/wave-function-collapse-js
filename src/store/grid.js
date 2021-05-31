02c605838cadf9e6c4ec922f9217ace69f9b8946
Deohgu <dioggopsanttos@gmail.com>
2021-05-31 19:25:03 +0100

    Revert "Adds TODO reminder"
    This reverts commit 30c3a336f2ab88be437b43472c64203e10eeb4cf.

--- src/store/grid.js
+++ src/store/grid.js
@@ -12,9 +12,6 @@ export default {
     width: 4,
     height: 4,
     // grid: gridGenerator(this.width, this.height, store.getters.identitiesArray),
-    //  TODO:
-    //  Improve this convertion, we want an array with each identity as an object
-    //    Ideally use the getter in identities
     grid: gridGenerator(4, 4, Object.entries(identities.state())),
   }),
 };
