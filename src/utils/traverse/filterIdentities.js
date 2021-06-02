472faf019cc0f32a940fdd56017e998d1025bd3b
Deohgu <dioggopsanttos@gmail.com>
2021-06-02 21:50:31 +0100

    filterIdentities does not check already set blocks

--- src/utils/traverse/filterIdentities.js
+++ src/utils/traverse/filterIdentities.js
@@ -1,5 +1,5 @@
 export default (
-  direction,
+  currDirection,
   allDirections,
   conditionalDirections,
   array,
@@ -11,23 +11,28 @@ export default (
   let neighboursCueClone = JSON.parse(JSON.stringify(neighboursCue));
   //  Loops valid directions for this particular location and returns then as a string
 
-  conditionalDirections[direction].forEach((direction) => {
+  conditionalDirections[currDirection].forEach((direction) => {
     const thisY = [allDirections(y, x)[direction].y()];
     const thisX = [allDirections(y, x)[direction].x()];
 
     //  From the valid directions get the cell in that direction of original cell
     array[thisY][thisX].forEach((identity) => {
-      //  If this identity, in this cell, is not in the rules of the original cell of its allowed neighbours of that direction remove it
-      if (arrayClone[y][x][0][1].rules[direction].indexOf(identity[0]) === -1) {
-        //  If current identity can not be in that direction splice from cloned array
-        arrayClone[thisY][thisX].splice(
-          arrayClone[thisY][thisX].findIndex((eachId) => {
-            if (eachId[0] === identity[0]) {
-              return true;
-            }
-          }),
-          1
-        );
+      //  Does not check already selected identities in blocks
+      if (array[thisY][thisX].length > 1) {
+        //  If this identity, in this cell, is not in the rules of the original cell of its allowed neighbours of that direction remove it
+        if (
+          arrayClone[y][x][0][1].rules[direction].indexOf(identity[0]) === -1
+        ) {
+          //  If current identity can not be in that direction splice from cloned array
+          arrayClone[thisY][thisX].splice(
+            arrayClone[thisY][thisX].findIndex((eachId) => {
+              if (eachId[0] === identity[0]) {
+                return true;
+              }
+            }),
+            1
+          );
+        }
       }
     });
 
