d46c8ae5384191e808db03fbb8afe052abf6ab23
Deohgu <dioggopsanttos@gmail.com>
2021-06-07 21:41:18 +0100

    #7 Copies CLONE, runs logic IF CLONE length > 1

--- src/utils/traverse/filterIdentities.js
+++ src/utils/traverse/filterIdentities.js
@@ -1,11 +1,12 @@
 export default (direction, array, { y, x, currY, currX }) => {
   let arrayClone = JSON.parse(JSON.stringify(array));
-  const currOriginalCell = array[currY][currX];
-  const currClonedCell = arrayClone[currY][currX];
-  const currClonedCellRules = arrayClone[y][x][0][1].rules;
+  const currOriginalCell = arrayClone[currY][currX];
 
-  currOriginalCell.forEach((identity) => {
-    if (currOriginalCell.length > 1) {
+  if (currOriginalCell.length > 1) {
+    const currClonedCell = arrayClone[currY][currX];
+    const currClonedCellRules = arrayClone[y][x][0][1].rules;
+
+    currOriginalCell.forEach((identity) => {
       //  If this identity is not a valid neighbour (i.e not in the rules), remove it
       if (currClonedCellRules[direction].indexOf(identity[0]) === -1) {
         currClonedCell.splice(
@@ -17,7 +18,7 @@ export default (direction, array, { y, x, currY, currX }) => {
           1
         );
       }
-    }
-  });
+    });
+  }
   return arrayClone;
 };
