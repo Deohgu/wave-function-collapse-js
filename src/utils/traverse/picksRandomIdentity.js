a29775ec86701cdb84dd00065320de4145a1ce9c
Deohgu <dioggopsanttos@gmail.com>
2021-06-07 21:39:40 +0100

    #7 Calculates random index based on CURRENT Cell length

--- src/utils/traverse/picksRandomIdentity.js
+++ src/utils/traverse/picksRandomIdentity.js
@@ -2,7 +2,7 @@ export default (array, { y, x }) => {
   let arrayClone = JSON.parse(JSON.stringify(array));
 
   if (array[y][x].length > 1) {
-    const randomIdentityIndex = Math.floor(Math.random() * array[y][1].length);
+    const randomIdentityIndex = Math.floor(Math.random() * array[y][x].length);
 
     arrayClone[y][x] = arrayClone[y][x].slice(
       randomIdentityIndex,
