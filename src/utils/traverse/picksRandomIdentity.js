f465f25b3311ebac6ab283466bdcc265afdce475
Deohgu <dioggopsanttos@gmail.com>
2021-06-11 10:47:33 +0100

    Reads current instead of original

--- src/utils/traverse/picksRandomIdentity.js
+++ src/utils/traverse/picksRandomIdentity.js
@@ -1,8 +1,10 @@
 export default (array, { y, x }) => {
   let arrayClone = JSON.parse(JSON.stringify(array));
 
-  if (array[y][x].length > 1) {
-    const randomIdentityIndex = Math.floor(Math.random() * array[y][x].length);
+  if (arrayClone[y][x].length > 1) {
+    const randomIdentityIndex = Math.floor(
+      Math.random() * arrayClone[y][x].length
+    );
 
     arrayClone[y][x] = arrayClone[y][x].slice(
       randomIdentityIndex,
