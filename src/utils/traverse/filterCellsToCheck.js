0559f6e1c95339289a2b159c256018ed1739f35d
Deohgu <dioggopsanttos@gmail.com>
2021-06-13 14:46:51 +0100

    #9 Creates array of Cells to check in own file

---
+++ src/utils/traverse/filterCellsToCheck.js
@@ -0,0 +1,31 @@
+import {
+  allDirections,
+  validSearchDirections,
+  conditionalDirections,
+} from "@/utils/traverse/directions";
+
+export default (array, y, x) => {
+  const filteredCellsCoordsArray = [];
+
+  conditionalDirections(y, x, array).forEach((isCorrectGridArea, index) => {
+    if (isCorrectGridArea) {
+      const directionNames = Object.keys(validSearchDirections);
+      const currValidSearchDirections =
+        validSearchDirections[directionNames[index]];
+
+      console.log(directionNames[index]);
+
+      //  Checks valid neighbouring cells
+      currValidSearchDirections.forEach((direction) => {
+        const currY = [allDirections(y, x)[direction].y()];
+        const currX = [allDirections(y, x)[direction].x()];
+        const currCell = array[currY][currX];
+
+        if (currCell.length > 1) {
+          filteredCellsCoordsArray.push({ y: currY, x: currX });
+        }
+      });
+    }
+  });
+  return filteredCellsCoordsArray;
+};
