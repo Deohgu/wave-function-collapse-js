c06ae55db13cd8767c7541d90f02ac542ba49203
Deohgu <dioggopsanttos@gmail.com>
2021-06-20 17:40:01 +0100

    Value wrongly encapsulated as array

--- src/utils/traverse/filterCellsToCheck.js
+++ src/utils/traverse/filterCellsToCheck.js
@@ -13,12 +13,12 @@ export default (array, y, x) => {
       const currValidSearchDirections =
         validSearchDirections[directionNames[index]];
 
-      console.log(directionNames[index]);
+      // console.log(directionNames[index]);
 
       //  Checks valid neighbouring cells
       currValidSearchDirections.forEach((direction) => {
-        const currY = [allDirections(y, x)[direction].y()];
-        const currX = [allDirections(y, x)[direction].x()];
+        const currY = allDirections(y, x)[direction].y();
+        const currX = allDirections(y, x)[direction].x();
         const currCell = array[currY][currX];
 
         if (currCell.length > 1) {
